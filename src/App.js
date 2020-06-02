d384433ce5a58a49c81e58306356a4ffbe9fcef7
Ismael Guimarães <56137836+ismaelguimaraes@users.noreply.github.com>
2020-06-02 14:00:12 -0300

    Ajustes no App

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import Routes from './routes';
 import GlobalStyle from './styles/global';
-import Header from './components/Header';
+import Header from './components/header';
 import FooterPage from './components/Footer';
 
 function App() {
